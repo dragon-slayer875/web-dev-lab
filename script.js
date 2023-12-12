c1ca00aa8b3c0e23242d44c8cfc945f8d776bef6
Yttrium_32 <shresthprasad7@gmail.com>
2023-12-12 09:20:45 +0530

    change active to nav-active

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const navBarElems = Array.from(document.querySelector(".navbar").children);
 navBarElems.forEach((elem) => {
     elem.addEventListener("click", () => {
         navBarElems.forEach((elem) => {
-            elem.classList.remove("active");
+            elem.classList.remove("nav-active");
         });
-        elem.classList.add("active");
+        elem.classList.add("nav-active");
         location.href = `${elem.id}.html`;
     });
 });
